ffe223f8f259e7f7375b778b43f043d313caffb9
BanLi-Official <94674279+BanLi-Official@users.noreply.github.com>
2024-06-05 11:43:28 +0800

    2024-6-5-11:43（labVersion）
    重新捡起来了

--- http_reactor/build/src/CMakeFiles/main.dir/Dispatcher.c.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/main.dir/Dispatcher.c.o: \
- /mnt/hgfs/VMshared/code/c++/HTTPReactor/http_reactor/src/Dispatcher.c \
- /usr/include/stdc-predef.h
--- http_reactor/src/HTTPRequest.c
+++ http_reactor/src/HTTPRequest.c
@@ -105,6 +105,27 @@ char* getHttpHeadValue(struct httpRequest* request ,const char* key)
     return NULL;
 }
 
+//编写一个函数，功能要求：以某一个字符串分割headline中的元素，分别将元素存储在request结构体的对应位置，并返回一次分割后的剩余内容起始位置
+char* splitHeadLine(char* start , char* end , const char* substr , char** ptr)
+{
+    char* subLocation=end;
+    if(substr != NULL)
+    {
+        subLocation = strstr(start , substr); //查找子字符串的位置
+        if(subLocation == NULL)
+        {
+            printf("splitHeadLine Error! space == NULL\n");
+        }
+    }
+    int containtLength = end-subLocation;    //计算分割的内容的长度
+    char* temp=(char *)malloc(containtLength+1);
+    strncpy(temp,start,containtLength);     //将内容暂时存储到一个临时变量中
+    temp[containtLength]='\0';
+    start = subLocation+1;
+    *ptr=temp;                              //将被分割的内容赋值到对应的目标位置，这里的ptr是一个二级指针
+    return start;
+} 
+
 bool parseHeadLine(struct httpRequest *request,struct buffer *readBuffer)
 {
     //   get /xxxxx/xxxx http/1.1
@@ -112,6 +133,25 @@ bool parseHeadLine(struct httpRequest *request,struct buffer *readBuffer)
     char *end = findFirstLine(readBuffer);
     char *start=readBuffer->data+readBuffer->readPos;
     int headLineSize = end - start;
+
+    //代码优化版本
+    if(headLineSize)
+    {
+        start = splitHeadLine(start,end," ",&request->method);    //解析方法
+        start = splitHeadLine(start,end," ",&request->URL);       //解析文件地址
+        splitHeadLine(start,end,NULL,&request->httpVersion);      //解析http版本
+
+        //为后面解析请求头做准备
+        request->parseState = parseHead;
+        readBuffer->readPos += headLineSize;
+        readBuffer->readPos += 2;
+
+        return true;
+    }
+
+
+#if 0
+    //粗略版本
 
     if(headLineSize) //开始解析第一行
     {
@@ -146,7 +186,7 @@ bool parseHeadLine(struct httpRequest *request,struct buffer *readBuffer)
 
         return true;
     }
-
+#endif
 
     return false;
 }
